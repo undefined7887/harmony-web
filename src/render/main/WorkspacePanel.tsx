43ec8ceb37b7c49890ca9c617ece3ea54a527b73
undefined7887 <undefined7887@gmail.com>
2023-04-10 01:32:13 +0300

    Fixed typing

--- src/render/main/WorkspacePanel.tsx
+++ src/render/main/WorkspacePanel.tsx
@@ -168,6 +168,12 @@ function Input({onText, onTyping}: InputProps) {
     function onKeyDown() {
         let now = new Date().getTime()
 
+        if (lastTyping == 0) {
+            setLastTyping(now)
+
+            return
+        }
+
         if (now - lastTyping > TYPING_TIMEOUT) {
             onTyping()
 
